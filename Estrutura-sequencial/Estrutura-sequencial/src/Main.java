8ef1471a80e5ad8a2b0aaa0435b26ab35e03d14b
Bruno Camilo da Silva <b.camilosilva16@gmail.com>
2026-06-16 18:07:10 -0300

    feat: add a new chapter: Sequential structure

---
+++ Estrutura-sequencial/Estrutura-sequencial/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.ENGLISH);
+        System.out.print("Olá mundo!");
+        System.out.println(" Bom dia!");
+
+        int y = 32;
+        double x = 10.35784;
+
+        System.out.println(y);
+        System.out.println(x);
+        System.out.printf("%.2f%n", x);
+        System.out.printf("%.4f%n", x);
+        System.out.println("Resultado = " + x + " Metros" );
+        System.out.printf("Resultado = %.2f Metros%n", x);
+
+        String nome = "Maria";
+        int idade = 35;
+        double renda = 4000.0;
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+    }
+}
